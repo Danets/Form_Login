86972afd78087bd7705e77d04d0ed3a81a186a64
Valeriy Danets <dantesvv@gmail.com>
2020-04-22 21:44:15 +0300

    added markup and styles for form

---
+++ app/js/app.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+const h1 = document.querySelector("h1");
+h1.addEventListener("click", () => {
+	h1.innerHTML = "Buy";
+})
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+How to start app: 
+1) Install Node Modules: npm i
+2) Run: gulp
